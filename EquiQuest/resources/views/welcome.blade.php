25052bd0f66b6a26b562fb3ed58bee4053f7d4be
Fathima-Amna <CB010707@students.apiit.lk>
2023-09-07 15:52:24 +0530

    change font-size

--- EquiQuest/resources/views/welcome.blade.php
+++ EquiQuest/resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
             <div class="text-center">
                 <img src="{{ asset('images/logo.png') }}" alt="logo" height="350" style="margin-right: 10px;">
                 <!-- Paragraph about the app -->
-                <p style="color: #000000;" class="mb-4">Welcome to EquiQuest, your stable management app.</p>
+                <p style="color: #000000;" class="mb-4 fs-2">Welcome to EquiQuest, your stable management app.</p>
                 <!-- Get Started Button -->
                 <a href="{{ route('register') }}" class="btn btn-primary" style="background-color: #454545;">Get Started</a>
             </div>
