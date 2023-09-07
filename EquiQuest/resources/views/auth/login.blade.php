19954d5c0648bb15221504fbfabbe523ad710b91
Fathima-Amna <CB010707@students.apiit.lk>
2023-09-07 16:18:55 +0530

    change css

--- EquiQuest/resources/views/auth/login.blade.php
+++ EquiQuest/resources/views/auth/login.blade.php
@@ -53,7 +53,7 @@
 
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary" style="background-color: #454545; border: 1px solid #454545;">
                                     {{ __('Login') }}
                                 </button>
 
